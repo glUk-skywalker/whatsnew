fd2593e3172415a75615bda5a4d74780c7f6a504
glUk <gluk.main+github@gmail.com>
2016-09-11 17:03:22 +0300

    missed file for login

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :ldap, Settings.ldap.to_hash
+end
