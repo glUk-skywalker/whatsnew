1bf9dbf8a21438082e445ba9b946ad91aa9c5cb6
glUk <gluk.main+github@gmail.com>
2018-03-20 15:43:28 +0300

    BuildsHelper module - check_build helper (amended): now uses the class WhatsnewDownloader instead of the own code

--- app/helpers/builds_helper.rb
+++ app/helpers/builds_helper.rb
@@ -1,24 +1,8 @@
 module BuildsHelper
 
   def check_build(tag)
-    whatsnew_time = nil
-    Net::HTTP.start(Settings.base_url) do |http|
-      response = http.request_head("/paupdate/#{ tag }/whatsnew.txt")
-      whatsnew_time = DateTime.parse(response['Last-Modified'])
-    end
-
-    require 'open-uri'
-
-    new_build_number = open("#{ Settings.updates_url }/#{ tag }/buildno.txt").read
-
-    if !Build.any? || Build.find_by(number: new_build_number).nil?
-      content = open("#{ Settings.updates_url }/#{ tag }/whatsnew.txt").read
-      build_info = content.split(/Build:  6.[0,5].\d+/)[1]
-      bugs_n_tasks = build_info.scan(/\n[#,T]\d+/).map{ |b| b[1..-1] }
-      bugs = bugs_n_tasks.select{ |b| b[0] == '#' }.map{ |b| b[1..-1] }.join(',')
-
-      Build.create(number: new_build_number, tag: tag, bug_list: bugs, whatsnew_time: whatsnew_time)
-    end
+    last_build = WhatsnewDownloader.get_for('beta').parsed.first
+    Build.create(last_build) unless Build.find_by_number(last_build[:number])
 
     config = Rails.configuration.database_configuration
     db = config[Rails.env]["database"]
