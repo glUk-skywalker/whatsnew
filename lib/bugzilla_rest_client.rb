0e8bbefcdb4e33a0a6ee6fbb96e726239e3e29a7
glUk <gluk.main+github@gmail.com>
2018-03-21 15:55:05 +0300

    BugzillaRestClient class - Now will send login+password with every request (to get rid of need of logout) - initialize method (removed): excess with the new strategy - get method (amended): now appends login+password to the passed params; now class method - get_bugs method (amended): now formatting of the output hash is more compact

--- lib/bugzilla_rest_client.rb
+++ lib/bugzilla_rest_client.rb
@@ -6,29 +6,16 @@ class BugzillaRestClient
     password:  Settings.bugzilla.password
   }
 
-  def initialize
-    resp = RestClient.get BASE_URL + '/rest.cgi/login', { params: CREDENTIALS }
-    resp_hash = JSON.parse resp.body
-    @token = resp_hash['token']
-  end
-
-  def get_bugs(bugs_list)
-    get('/bug', { id: bugs_list.join(",") })['bugs'].map{ |b|
-      {
-        id: b['id'],
-        summary: b['summary'],
-        status: b['status'],
-        assignments: '',
-        included: '',
-        product: b['product']
-      }
+  def self.get_bugs(bugs_list)
+    get('/bug', { id: bugs_list })['bugs'].each{ |b|
+      b.slice! *%w(id summary status creator cc qa_contact product)
     }
   end
 
   private
 
-  def get(url, params)
-    params[:token] = @token
+  def self.get(url, params)
+    params.merge! CREDENTIALS
     resp = RestClient.get BASE_URL + '/rest.cgi' + url, { params: params }
     JSON.parse resp.body
   end
