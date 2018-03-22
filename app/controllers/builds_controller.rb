fb15f9b57eef32922d4ecdc54be8e1a0e78cd32c
glUk <gluk.main+github@gmail.com>
2018-03-22 15:21:34 +0300

    BuildsController class amended to use the new code and refactored Build views templates  are adjusted to complement the new controller code

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -1,68 +1,23 @@
 class BuildsController < AuthenticatedUserController
-  include BuildsHelper
+
+  NOT_PROCESSED_BUG_STATUSES = %w(RESOLVED TO-VERIFY TO-DOCUMENT")
 
   def index
     @builds = Build.order('whatsnew_time DESC')
   end
 
-  # def check
-  #   check_build(params[:tag])
-  #
-  #   redirect_to root_path
-  # end
-
   def show
     @build = Build.find(params[:id])
 
-    last_commits = {}
-
-    @build.bugs_info.each do |bug_history|
-      date = time = nil
-      (0..(bug_history[:history].length - 1)).reverse_each.each do |i|
-        if reporev_changed?(bug_history[:history][i])
-          date ||= bug_history[:history][i][:when].to_date
-          time ||= bug_history[:history][i][:when].to_time
-        end
-      end
-
-      if !date.nil?
-        last_commits[ bug_history[:id] ] = DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec)
-      end
-    end
-
-
-    @bugs = []
-    @build.bugs_info.each do |bug|
-      if last_commits[bug[:id]].nil?
-        included = true
-      else
-        @build.whatsnew_time > last_commits[bug[:id]] ? included = true : included = false
-      end
-
-      @bugs << bug.merge({ included: included })
-    end
-
-    processed = true
-    @bugs.each do |bug|
-      processed = false if (bug[:status] == "RESOLVED" || bug[:status] == "TO-VERIFY" || bug[:status] == "TO-DOCUMENT") && bug[:included]
-    end
-    @build.update_attributes(processed: processed)
+    not_processed_bugs = @build.bugs_info.select{ |b|
+      b[:status].in?(NOT_PROCESSED_BUG_STATUSES) && b[:included]
+    }
+    @build.update_attributes(processed: true) if not_processed_bugs.empty?
 
     respond_to do |format|
       format.js { authenticated? }
       format.html { redirect_to root_path }
     end
-  end
-
-
-protected
-
-  def reporev_changed?(change)
-    res = false
-    change[:changes].each do |change|
-      res = true if change.has_value?('cf_reporev') ||change.has_value?('whiteboard')
-    end
-    res
   end
 
 end
