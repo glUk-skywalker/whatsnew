f5a9a461a6dcfa67ced516ea08320ca45b2e5131
glUk <gluk.main+github@gmail.com>
2018-03-22 11:15:06 +0300

    Build class - bugs_info method (new):   retrieves bugs info and history from bugzilla   joins the retrieved data in a singe array   adjusts reletaions to tester   filters out the bugs with NEW status   All above steps are skipped if the information already presetns

--- app/models/build.rb
+++ app/models/build.rb
@@ -1,2 +1,12 @@
 class Build < ActiveRecord::Base
+
+  def bugs_info
+    return @bugs_info if @bugs_info                                             # avoid extra requests if information exists
+    bugs = BugzillaClient.get_bugs bug_list
+    bugs_history = BugzillaClient.get_bugs_history bug_list
+    info = (bugs + bugs_history).group_by{ |info| info["id"] }.map{ |k, v| v.reduce(:merge) }
+    @bugs_info = info.map{ |b| Bug.new(b).adjust_tester_relations!.to_h }
+    @bugs_info.delete_if{ |b| b[:status] == 'NEW' }
+  end
+
 end
