4c28fa454bd3b984b92d300bcc5d8ec9e30edbe6
glUk <gluk.main+github@gmail.com>
2018-03-22 15:03:29 +0300

    Bug class - initalize method (amended):   total_symbolize_keys! method now called right in the constructor (rest of methods are amended to use symbols instead of keys for hashes)   adjust_testers_relations! called here as well - to_h method (amended):   total_symbolize_keys! call removed - adjust_presence_in! method (new): sets @info[:included] = true if the bug presents in the passed build - adjust_tester_relations! method (amended):   now uses symbols as hash keys   return self call removed (as it is now private method) - last_commit_time method (new): returns the time of last commit (nil if there are no new commits)

--- app/models/build.rb
+++ app/models/build.rb
@@ -5,7 +5,7 @@ class Build < ActiveRecord::Base
     bugs = BugzillaClient.get_bugs bug_list
     bugs_history = BugzillaClient.get_bugs_history bug_list
     info = (bugs + bugs_history).group_by{ |info| info["id"] }.map{ |k, v| v.reduce(:merge) }
-    @bugs_info = info.map{ |b| Bug.new(b).adjust_tester_relations!.to_h }
+    @bugs_info = info.map{ |b| Bug.new(b).adjust_presence_in!(self).to_h }
     @bugs_info.delete_if{ |b| b[:status] == 'NEW' }
   end
 
--- lib/bug.rb
+++ lib/bug.rb
@@ -2,15 +2,34 @@ class Bug
 
   def initialize(info)
     @info = info
+
+    @info.total_symbolize_keys!
+    adjust_tester_relations!
   end
+
+  def adjust_presence_in!(build)
+    if last_commit_time
+      @info[:included] = build.whatsnew_time > last_commit_time
+    else
+      @info[:included] = true
+    end
+
+    self
+  end
+
+  def to_h
+    @info
+  end
+
+  private
 
   def adjust_tester_relations!
     relations = []
     Settings.testers.each do |tester|
       relation = []
-      relation << 'creator' if @info['creator'] == tester
-      relation << 'CC' if @info['cc'].include? tester
-      relation << 'qa contact' if @info['qa_contact'] == tester
+      relation << 'creator' if @info[:creator] == tester
+      relation << 'CC' if @info[:cc].include? tester
+      relation << 'qa contact' if @info[:qa_contact] == tester
 
       unless relation.empty?
         relations << {
@@ -19,12 +38,19 @@ class Bug
         }
       end
     end
-    @info['relations'] = relations
-    return self
+    @info[:relations] = relations
   end
 
-  def to_h
-    @info.total_symbolize_keys!
+  def last_commit_time
+    commits_history = @info[:history].select{ |event|                           # filtering out inessential items
+      event[:changes].any?{ |c|
+        c.has_value?('cf_reporev') || c.has_value?('whiteboard')
+      }
+    }
+
+    return nil unless commits_history.any?
+
+    commits_history.sort{ |a, b| b[:when].to_time <=> a[:when].to_time }.first[:when].to_time
   end
 
 end
