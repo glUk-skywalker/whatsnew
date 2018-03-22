029830765bfe1124cba44b3d3f40331255954d24
glUk <gluk.main+github@gmail.com>
2018-03-22 11:17:07 +0300

    The controller and view are amended to use the new code

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -5,62 +5,41 @@ class BuildsController < AuthenticatedUserController
     @builds = Build.order('whatsnew_time DESC')
   end
 
-  def check
-    check_build(params[:tag])
-
-    redirect_to root_path
-  end
+  # def check
+  #   check_build(params[:tag])
+  #
+  #   redirect_to root_path
+  # end
 
   def show
     @build = Build.find(params[:id])
 
-    require 'xmlrpc/client'
-    require 'openssl'
-
-    server = XMLRPC::Client.new2("#{ Settings.bugzilla_url }/xmlrpc.cgi")
-    server.instance_variable_get(:@http).instance_variable_set(:@verify_mode, OpenSSL::SSL::VERIFY_NONE)
-
-    bug = server.proxy 'Bug'
-
     last_commits = {}
-    bug.history(ids: @build.bug_list.split(','), Bugzilla_login: Settings.bugzilla.login, Bugzilla_password: Settings.bugzilla.password)['bugs'].each do |bug_history|
+
+    @build.bugs_info.each do |bug_history|
       date = time = nil
-      (0..(bug_history['history'].length - 1)).reverse_each.each do |i|
-        if reporev_changed?(bug_history['history'][i])
-          date ||= bug_history['history'][i]['when'].to_date
-          time ||= bug_history['history'][i]['when'].to_time
+      (0..(bug_history[:history].length - 1)).reverse_each.each do |i|
+        if reporev_changed?(bug_history[:history][i])
+          date ||= bug_history[:history][i][:when].to_date
+          time ||= bug_history[:history][i][:when].to_time
         end
       end
 
       if !date.nil?
-        last_commits[ bug_history['id'] ] = DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec)
+        last_commits[ bug_history[:id] ] = DateTime.new(date.year, date.month, date.day, time.hour, time.min, time.sec)
       end
     end
 
 
     @bugs = []
-    bug.get(ids: @build.bug_list.split(','), Bugzilla_login: Settings.bugzilla.login, Bugzilla_password: Settings.bugzilla.password)['bugs'].each do |bug|
-      assignments = []
-      Settings.testers.each do |tester|
-        assignment = []
-        assignment  << 'creator' if bug['creator'] == tester
-        assignment << 'CC' if bug['cc'].include? tester
-        assignment << 'qa contact' if bug['qa_contact'] == tester
-
-        if !assignment.empty?
-          assignments << { tester: tester, assignment: assignment.join(', ') }
-        end
-      end
-
-      if last_commits[bug['id']].nil?
+    @build.bugs_info.each do |bug|
+      if last_commits[bug[:id]].nil?
         included = true
       else
-        @build.whatsnew_time > last_commits[bug['id']] ? included = true : included = false
+        @build.whatsnew_time > last_commits[bug[:id]] ? included = true : included = false
       end
 
-      unless bug['status'] == 'NEW'
-        @bugs << { id: bug['id'], summary: bug['summary'], status: bug['status'], assignments: assignments, included: included, product: bug['product'] }
-      end
+      @bugs << bug.merge({ included: included })
     end
 
     processed = true
@@ -80,7 +59,7 @@ protected
 
   def reporev_changed?(change)
     res = false
-    change['changes'].each do |change|
+    change[:changes].each do |change|
       res = true if change.has_value?('cf_reporev') ||change.has_value?('whiteboard')
     end
     res
