2be0ba860f454a0d7b89c34e9c515efc6460a688
glUk <gluk.main+github@gmail.com>
2018-12-26 15:21:59 +0300

    phab_auth is updated + its config is updated as well

--- config/initializers/phab_auth.rb
+++ config/initializers/phab_auth.rb
@@ -3,4 +3,5 @@ PhabAuth.setup do |conf|
   conf.client_secret = Settings.phabricator.login.client_secret
   conf.oauthserver_url = Settings.phabricator.login.oauthserver_url
   conf.create_session_path = :create_session_path
+  conf.scheme = 'http'
 end
